1ba851ecf8f684d731bae3394cd4b18e86386df4
kimute <jkdnekki@icloud.com>
2022-06-16 23:51:49 +0900

    modify index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import { App } from './components/app';
 import reportWebVitals from './reportWebVitals';
 import './styles/styles.css';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './apollo';
 import { HelmetProvider } from 'react-helmet-async';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement!);
-
-root.render(
-  <ApolloProvider client={client}>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </ApolloProvider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 reportWebVitals();
